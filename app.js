6d75e8660408e3e21f40171c6b19476e6bc17313
unknown <ishpreetshriya@gmail.com>
2023-02-12 17:54:13 +0530

    fixed a bug

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const https = require("https")
 app.get("/", function(req,res)
 {
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=6ce56cdf9e1708947494bd1d2c6fcce6" ;
+    const url = "https://api.openweathermap.org/data/2.5/weher?lat=44.34&lon=10.99&appid=6ce56cdf9e1708947494bd1d2c6fcce6" ;
 
-    https.get(url, function(response)
-        console.log(response);
+    https.get(url, function(response){
+        
+        console.log(response.statusCode);
     })
 
 
